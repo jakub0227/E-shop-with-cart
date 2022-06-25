dd5c552cb777c92b1fcf88040a8ee7eec639f241
jakub0227 <jakub.wysocki.1983@gmail.com>
2022-06-25 10:47:18 +0200

    fix flex in card.title

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -18,9 +18,9 @@ export const StoreItem: FC<StoreItemProps> = ({ id, name, price, imgUrl }) => {
         style={{ objectFit: "cover" }}
       />
       <Card.Body className="d-flex flex-column">
-        <Card.Title className="d-flex justify-content-space-between align-items-baseline mb-4">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
           <span className="fs-2">{name}</span>
-          <span className="ms-2 text-muted">{price} PLN</span>
+          <span className="ms-2 text-muted">{price}</span>
         </Card.Title>
       </Card.Body>
     </Card>
