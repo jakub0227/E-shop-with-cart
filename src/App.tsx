1fa926653fcfac609f2ca506ebee05d48470b329
jakub0227 <jakub.wysocki.1983@gmail.com>
2022-06-25 09:49:14 +0200

    add Navigation mockup

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { Container } from "react-bootstrap";
 import { HomePage } from "./pages/HomePage";
 import { StorePage } from "./pages/StorePage";
 import { AboutPage } from "./pages/AboutPage";
+import { Navigation } from "./components/Navigation";
 
 export const App = () => {
   return (
-    <Container>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/store" element={<StorePage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
-    </Container>
+    <>
+      <Navigation />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/store" element={<StorePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </Container>
+    </>
   );
 };
