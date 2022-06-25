44f32284b06efeeab25e2f5d55dc11ee27409faf
jakub0227 <jakub.wysocki.1983@gmail.com>
2022-06-25 09:59:12 +0200

    add routes functionality, create route urls, map through routes array

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,38 @@
 import React from "react";
-import { Container, Navbar } from "react-bootstrap";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+interface Route {
+  name: string;
+  url: string;
+}
+const routes: Route[] = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Store",
+    url: "/store",
+  },
+  {
+    name: "About",
+    url: "/about",
+  },
+];
 
 export const Navigation = () => {
   return (
     <Navbar className="bg=white shadow-lg mb-3">
-      <Container>Navigation</Container>
+      <Container>
+        <Nav>
+          {routes.map((el, i) => (
+            <Nav.Link key={i} to={el.url} as={NavLink}>
+              {el.name}
+            </Nav.Link>
+          ))}
+        </Nav>
+      </Container>
     </Navbar>
   );
 };
